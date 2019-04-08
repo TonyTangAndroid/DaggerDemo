662e111b2ba945263c49bc0bf0e5b1ea59c4c5f6
TonyTandAndroid <tonytangandroid@gmail.com>
2019-04-07 22:13:19 -0700

    fix TeacherRepository.

--- app/src/test/java/demo/tang/tony/test/TeacherRepositoryTest.java
+++ app/src/test/java/demo/tang/tony/test/TeacherRepositoryTest.java
@@ -2,6 +2,8 @@ package demo.tang.tony.test;
 
 import com.google.common.truth.Truth;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -11,20 +13,45 @@ import demo.tang.tony.di.DaggerNetworkComponent;
 import demo.tang.tony.di.NetworkComponent;
 import demo.tang.tony.model.MockApiConstants;
 import demo.tang.tony.model.Teacher;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 
 public class TeacherRepositoryTest {
+
+    private TeacherApi teacherApi;
+
+    private MockWebServer mockWebServer;
+
+
+    @Before
+    public void setup() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+        teacherApi = DaggerNetworkComponent.builder().url(mockWebServer.url("/").toString()).build().teacherApi();
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        mockWebServer.shutdown();
+    }
 
 
     @Test
     public void get_by_id_should_return_student() throws IOException {
+        String json = TestUtils.json("teacher.json", this);
+        MockResponse mockResponse = new MockResponse().setResponseCode(200).setBody(json);
+        mockWebServer.enqueue(mockResponse);
+
+
+
         TeacherRepository studentRepository = new TeacherRepository(restApi());
         Teacher actual = studentRepository.get(MockApiConstants.TEACHER_ID);
-        Truth.assertThat(actual).isEqualTo(Teacher.builder().id(1).name("TonyTang").build());
+        Truth.assertThat(actual).isEqualTo(Teacher.builder().id(2).name("TonyTang").build());
     }
 
     private TeacherApi restApi() {
         NetworkComponent networkComponent = DaggerNetworkComponent.builder()
-                .url(MockApiConstants.SERVER_URL)
+                .url(mockWebServer.url("/").toString())
                 .build();
         return networkComponent.teacherApi();
     }
